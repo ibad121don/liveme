90e2db4872b2213de5ee0463ce4e5a60980fc5f2
Ibad qureshi <63195136+ibad121don@users.noreply.github.com>
2025-07-31 12:33:54 -0700

    Update useDashboardData.ts

--- hooks/dashboard/useDashboardData.ts
+++ hooks/dashboard/useDashboardData.ts
@@ -9,7 +9,7 @@ const fetchDashboardData = async (): Promise<DashboardApiResponse> => {
     throw new Error("Authentication token not found. Please log in.");
   }
 
-  const response = await fetch(`${BASE_URL}/api/super-admin/dashboard`, { 
+  const response = await fetch(`https://liveme-three.vercel.app/api/super-admin/dashboard`, { 
     method: "GET",
     headers: {
       "Content-Type": "application/json",
